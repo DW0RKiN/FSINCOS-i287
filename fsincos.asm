e8752ab3508a99369859f0e09133c4ca3bd5664d
DW0RKiN <ogion@centrum.cz>
2017-02-01 15:26:25 +0100

    Create fsincos.asm
    TASM

---
+++ fsincos.asm
@@ -0,0 +1,23 @@
+if i387 
+	FSINCOS			; +-cos, +-sin
+else
+	FLD	st		; x, x
+	FPTAN			; 1, tan(st0), x
+	FLD	st(1)		; tan, 1, tan, x
+	FMUL	st, st		; tan^2, 1, tan, x
+	FADDP	st(1), st	; 1+tan^2, tan, x
+	FSQRT			; (1+tan^2)^0.5, tan, x
+	FDIVR	st,st(1)	; sin, tan, x
+	FABS			; sin, tan, x
+	FLD1			; 1, sin, tan, x
+	FADD	st, st		; 2, sin, tan, x
+	FDIVR	st, st(3)	; x/2, sin, tan, x
+	FPTAN			; 1, tan pul, sin, tan, x
+	FSTP	st		; tan pul, sin, tan, x
+	FMUL	st(1), st	; tpul, sin * tpul, tan, x
+	FABS			; abs(tpul), sin * tpul, tan, x
+	FDIVP			; +-sin, tan, x
+	FSTP	st(2)		; tan, +-sin
+	FDIVR	st, st(1)	; +-cos, +-sin
+endif
+  
